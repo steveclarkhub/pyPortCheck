d0dde399eafaa475856f8b65917c480135753866
steve <steve@steves-MacBook-Pro.local>
2024-11-21 18:51:26 -0700

    renamed function params to server from hostname

--- port_check.py
+++ port_check.py
@@ -1,10 +1,10 @@
 import argparse 
 import socket  #standard lib
 
-def port_check(hostname, portno):
+def port_check(servername, portno):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(3)
-        con = s.connect_ex((hostname, portno))
+        con = s.connect_ex((servername, portno))
     return con 
 
 # connect_ex throw a code on exception instead of the exception itself
@@ -13,7 +13,7 @@ def port_check(hostname, portno):
 # print(con)
 
 parser = argparse.ArgumentParser(description="Process hostname and port.")
-parser.add_argument('-s', '--server', type=str, default='8.8.8.8', help='The server to connect to (default: 8.8.8.8)')
+parser.add_argument('-s', '--servername', type=str, default='8.8.8.8', help='The server to connect to (default: 8.8.8.8)')
 parser.add_argument('-p', '--port', type=int, default='443', help='The port number on target server (default: 443)')
 parser.add_argument('-f', '--file', type=str, help='path to the CSV file with a list of targets and ports')
 
@@ -28,7 +28,7 @@ if args.file:
      print("file flag present")
      print(args.file,"is the path")
 else:
-    output = port_check(args.hostname,args.port)
+    output = port_check(args.servername,args.port)
 
     if output == 0:
         print("return code: ", output, "success")
@@ -36,9 +36,5 @@ else:
         print("return code: ", output, "failure")
 
 
-     
-
-
-
-
+    
 
